a2271984c0f18ad71bfc01ceb70006cc75c22f30
Nick Pfisterer <nick@polymoon.net>
2017-09-24 18:06:29 -0700

    Fix missing comma

--- commands/jobs/lft.js
+++ commands/jobs/lft.js
@@ -25,7 +25,7 @@ module.exports = class LookingForTalentCommand extends Command {
   constructor(client) {
     super(client, {
       name: 'lft',
-      aliases: ['talent']
+      aliases: ['talent'],
       group: 'jobs',
       memberName: 'lft',
       description:
