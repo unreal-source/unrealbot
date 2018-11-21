49086793fe2c5375cc4f26a4829314fb7d1398d0
Nick Pfisterer <nick@polymoon.net>
2018-11-21 12:21:57 -0800

    Remove old jobs channel functionality

--- bot.js
+++ bot.js
@@ -49,16 +49,3 @@ client.on('ready', () => {
   log.info('Logged in!')
   client.user.setGame('Discord')
 })
-
-// NOTE: Temporarily restore old job post detection until the new features are done
-client.on('message', message => {
-  if (message.channel.name === 'jobs') {
-    const prefix = /^\*\*\[(paid|unpaid|royalty|contract)\]\*\*$/i
-
-    if (message.content.split(' ')[0].match(prefix)) {
-      message.pin()
-    }
-  } else {
-    return
-  }
-})
